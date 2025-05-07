adc77b8ce98ec8858093c974dd472cc2a04e73ca
changhee <jaduwvad2@gmail.com>
2025-05-07 10:43:50 +0900

    add magic number

--- Project21/similar_scoring.cpp
+++ Project21/similar_scoring.cpp
@@ -8,10 +8,14 @@ public:
 	int cmpLength(const string& str1, const string& str2) {
 		int gap = getGap(str1, str2);
 
-		return getScore(str1, str2, gap);
+		double score = getScore(str1, str2, gap);
+		if (0 > score) score = 0;
+
+		return score;
 	}
 
 private:
+	const int lengthMaxScore = 60;
 	int getGap(const string& str1, const string& str2) {
 		if (str1.length() > str2.length()) {
 			return str1.length() - str2.length();
@@ -21,7 +25,7 @@ private:
 
 	double getScore(const string& str1, const string& str2, int gap) {
 		if (0 == gap) 
-			return 60;
+			return lengthMaxScore;
 		else if (str1.length() > str2.length()) 
 			return (1 - (double)gap / str2.length()) * 60;
 		else 
